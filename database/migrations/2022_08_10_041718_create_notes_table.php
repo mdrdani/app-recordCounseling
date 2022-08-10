b19963a6e53fb2ac5f0fc414f43bb0bd6ae869fe
mdrdani <sudokill3004@gmail.com>
2022-08-10 14:18:05 +0700

    Buat Model Notes dan Migration

---
+++ database/migrations/2022_08_10_041718_create_notes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('siswa_id');
+            $table->date('tanggal');
+            $table->string('masalah')->nullable();
+            $table->string('penanganan')->nullable();
+            $table->string('foto')->nullable();
+            $table->timestamps();
+
+            $table->foreign('siswa_id')->references('id')->on('siswas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
